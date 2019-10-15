7df6c6ac4025d7c024bd9d39433a27aa0321c1f4
James Colvin <jamesc127@gmail.com>
2019-10-15 14:59:50 -0500

    switched output to tab delimited, adding join on clustering

--- src/main/scala/TableCompare.scala
+++ src/main/scala/TableCompare.scala
@@ -1,20 +1,23 @@
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import com.typesafe.config.ConfigFactory
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.functions.col
-import org.apache.spark.sql.types._
 
 object TableCompare {
   def main(args: Array[String]): Unit = {
 
     val spark             = SparkSession.builder.appName("Table Compare").getOrCreate()
-    val config            = ConfigFactory.load()
-    val master_table      = config.getString("master_table.table")
-    val master_keyspace   = config.getString("master_table.keyspace")
-    val compare_table     = config.getString("compare_table.table")
-    val compare_keyspace  = config.getString("compare_table.keyspace")
-    val t1_join           = config.getString("join_column.t1_name")
-    val t2_join           = config.getString("join_column.t2_name")
+//    val spark               = SparkSession.builder.appName("Table Compare").master("local").getOrCreate()
+    val config              = ConfigFactory.load()
+    val master_table        = config.getString("master_table.table")
+    val master_keyspace     = config.getString("master_table.keyspace")
+    val compare_table       = config.getString("compare_table.table")
+    val compare_keyspace    = config.getString("compare_table.keyspace")
+    val t1_join             = config.getString("join_column.t1_name")
+    val t2_join             = config.getString("join_column.t2_name")
+    val masterClustering1   = config.getString("master_table.clustering1")
+    val compareClustering1  = config.getString("compare_table.clustering1")
+    val masterClustering2   = config.getString("master_table.clustering2")
+    val compareClustering2  = config.getString("compare_table.clustering2")
 
     val columns = spark.read.format("org.apache.spark.sql.cassandra").options(Map("table" -> config.getString("system_table.table"), "keyspace" -> config.getString("system_table.keyspace"))).load()
     columns.createOrReplaceTempView("columns")
@@ -41,7 +44,10 @@ object TableCompare {
     t1.createOrReplaceTempView("t1")
     t2.createOrReplaceTempView("t2")
 
-    val results = spark.sql(s"""SELECT $select_clause_trim FROM t1 FULL OUTER JOIN t2 ON t1.$t1_join = t2.$t2_join""")
+    val clustering1Join = if (config.getString("master_table.clustering1") != null && config.getString("compare_table.clustering1") != null) s""" AND t1.$masterClustering1 = t2.$compareClustering1""" else null
+    val clustering2Join = if (config.getString("master_table.clustering2") != null && config.getString("compare_table.clustering2") != null) s""" AND t1.$masterClustering2 = t2.$compareClustering2""" else null
+
+    val results = spark.sql(s"""SELECT $select_clause_trim FROM t1 FULL OUTER JOIN t2 ON t1.$t1_join = t2.$t2_join $clustering1Join $clustering2Join""")
 
     def updateColumnTypes(df:DataFrame,columnIt:Iterator[String]):DataFrame = {
       def dfHelper(dFrame:DataFrame,col:String):DataFrame = {
@@ -52,6 +58,6 @@ object TableCompare {
     }
 
     val resultsString = updateColumnTypes(results,results.columns.toIterator)
-    resultsString.coalesce(1).write.option("header","true").csv(config.getString("csv_path.output_path"))
+    resultsString.coalesce(1).write.option("header","true").option("delimiter", "\t").option("quote", "\u0000").csv(config.getString("csv_path.output_path"))
   }
 }
